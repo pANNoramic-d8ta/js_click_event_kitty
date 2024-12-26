9f518d219aea09eb6dfa2e7ab0450c2f3b760f9b
pANNoramic-d8ta <pannoramicd8ta@duck.com>
2024-12-25 19:52:58 -0500

    Added if & else statements to the image / button function.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 var button = document.querySelector(".show-cat");
-//console.log(button);
 var cat = document.querySelector(".cat");
-//console.log(cat);
 
-button.addEventListener("click", function
-    () {
-    //console.log("It's working");
+button.addEventListener("click", function () {
+    //cat.classList.add("show")
 
+    if (cat.classList.contains("show")) {
+        button.classList.add("disappear")
+    //console.log("Yes");
+    cat.classList.remove("show")
+    button.innerText = ("Wait, come back!");
+} else {
+    //console.log("No");
     cat.classList.add("show")
-});
+    button.innerText = ("Shoo,cat!!");
+    button.classList.remove("disappear")
+}
+    });
